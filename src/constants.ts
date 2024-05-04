7633bfd7133661b47e9d5cd8fef098479cf90a31
saadfrhan <saadfarhan347@gmail.com>
2024-05-04 06:28:46 +0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -3,23 +3,19 @@ export const SCALE_FACTOR = 4;
 export const DIALOG_DATA: {
   [x: string]: any;
 } = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days. I'm exploring FastAPI and Docker these days. I am also up for CLI development in Node.js.
+    Here is my <a href="https://github.com/saadfrhan" target="_blank">Github</a>!`,
+  "cs-degree": `This is my future CS degree. I'm currently studying Computer Science at the Virtual University of Pakistan.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. I also like to read books here. I'm currently reading "The Pragmatic Programmer".`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my resume. <a href="https://www.canva.com/design/DAFb3337tIA/nh3rOWE5EiM7U2QB5yQK5A/edit" target="_blank">Check it out?</a>
+  Contact me at saadfarhan347@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  exit: `If you want to exit saadfrhan's portfolio, just close the tab.`,
 };
